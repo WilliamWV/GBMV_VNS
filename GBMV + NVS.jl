03fbbb191a9831d956f7db70539ecb4e26975cc7
WilliamWV <william.w.vargas@gmail.com>
2018-05-24 22:32:24 -0300

    created an instance type that reads an instance from file

--- GBMV + NVS.jl
+++ GBMV + NVS.jl
@@ -50,8 +50,9 @@
 
 #Ideias para vizinhança - Todas assumindo que a solução se mantém viável - em ordem crescente de número de vizinhos no caso médio:
     # 0) 
-        #para grupos adjacentes(grupos com aresta compartilhada) troca um dos participantes da aresta de maior valor
-        #compartilhada entre os grupos : O(g*g) vizinhos (grupos compartilhados -> g*(g-1)/2 com grupos totalmente conexos)
+        #para cada possível dupla de grupos troca um dos
+        #participantes da aresta compartilhada de maior valor
+        #O(g*g) vizinhos 
     # 1) 
         #Para cada grupo, trocar o vértice v cujo aproveitamento precentual é menor, ou seja o vértice cuja soma das arestas 
         #que participa que são internas ao grupo divida pela soma total dos valores das arestas seja menor O(g*g)
@@ -64,5 +65,51 @@
     
 #OBS sobre as vizinhanças : A vizinhança 0 está contida na vizinhança 2 assim como a vizinhança 1 está contida na vizinhança 3 elas foram separadas pois as vizinhanças 0 e 1 fazem considerações mais específicas em relação a função objetivo, logo tendem a poder melhorar o seu valor sem ter custos computacionais tão altos quanto a vizinhança 2 e 3, por esse motivo elas executarão antes e se usará a ideia de first improvement
 
+#Uma instância do problema contém:
+    # n = número de vértices
+    # g = número de grupos
+    # L[g] = limite inferior de cada grupo
+    # U[g] = limite superior de cada grupo
+    # P[n] = pesos de cada vértice
+    # A[n,n] = peso da aresta entre o vértice os vértices
 
+type Instance
+    n :: Int64
+    g :: Int64
+    L
+    U
+    P
+    A
+    function Instance(filename)
+        open(filename) do f
+            lines = readlines(f)
+            first_line = split(lines[1])
+            n = parse(Int64, first_line[1])
+            g = parse(Int64, first_line[2])
+            second_line = split(lines[2])
+            L = zeros(g)
+            U = zeros(g)
+            for i=1:g
+                L[i] = parse(Float64, second_line[i*2-1])
+                U[i] = parse(Float64, second_line[i*2])
+            end
+            P = zeros(n)
+            third_line = split(lines[3])
+            for i=1:n
+                P[i] = parse(Float64, third_line[i])
+            end
+            A = zeros(n,n)
+            for i =4:first(size(lines))
+                line = split(lines[i])
+                u = parse(Int64,line[1])
+                v = parse(Int64,line[2])
+                value = parse(Float64, line[3])
+                A[u+1,v+1] = value
+            end
+            
+            new(n, g, L, U, P, A)
+        end
+    end
+end
 
+Instance("gbmv240_01.ins")
